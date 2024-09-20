f77d85ce9ecf6c56c991b2b081a77eb40b9923a8
Louise Eriksson <louiseriksson.92@gmail.com>
2024-09-20 22:40:54 +0200

    Done with round rotate card

--- src/components/RotateCard/RoundCard.jsx
+++ src/components/RotateCard/RoundCard.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import styles from "./RotateCard.module.css";
 
 const RoundCard = () => {
-  return <div>RoundCard</div>;
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardSideFront}>{/* <div>Front Side</div> */}</div>
+      <div className={styles.cardSideBack}>
+        <a href="">Link to side</a>
+      </div>
+    </div>
+  );
 };
 export default RoundCard;
